f233ac97a78621fd57dda6d0590d70e5d9818680
Sergio Alejandro Garcia Sosa <garciasosass583@gmail.com>
2026-01-07 23:17:53 -0500

    Se modifco dasboard con nuevo diseño mas responsivo y limpio y productos ya esta listo con su crud

--- usuarios/views.py
+++ usuarios/views.py
@@ -168,7 +168,7 @@ def lista_usuarios_view(request):
         'filtro_activo': filtro_activo,
         'filtro_staff': filtro_staff,
     }
-    return render(request, 'usuarios/panel_admin/lista_usuarios.html', context)
+    return render(request, 'usuarios/lista_usuarios.html', context)
 
 
 @login_required
