b44c9772879ae953a3905b232ccc772041814fc3
astrid carolina <astridcarolina.jaime@gmail.com>
2026-01-19 16:16:11 -0500

    creacion de aplicacion clientes

--- MONAPP/urls.py
+++ MONAPP/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,  include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('auth/', include('usuarios.urls')),
+    # path('auth/', include('usuarios.urls')),
+    path('clientes/', include('clientes.urls')),
 ]
