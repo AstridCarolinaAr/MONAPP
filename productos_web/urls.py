18379ef461e6158629abeaca8a2670669988753e
Александр <Alejandrod1707@gmail.com>
2026-03-07 07:13:39 -0500

    Actualización del Dashboarddddd y cambios en validaciones de tiempo real

--- productos_web/urls.py
+++ productos_web/urls.py
@@ -6,6 +6,7 @@ app_name = 'productos_web'
 urlpatterns = [
     path('', views.lista_productos_web, name='lista'),
     path('crear/', views.crear_producto_web, name='crear'),
+    path('api/detalle/<uuid:pk>/', views.detalle_producto_web_json, name='detalle_json'),
     path('editar/<uuid:pk>/', views.editar_producto_web, name='editar'),
     path('eliminar/<uuid:pk>/', views.eliminar_producto_web, name='eliminar'),
     path('toggle/<uuid:pk>/', views.toggle_visible, name='toggle_visible'),
